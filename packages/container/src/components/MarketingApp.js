c43ddb6ed37ff14fe136654b3ae844bb0053d4ef
Fadhli Syaifullah <fadhlisyaifullah@gmail.com>
2022-05-28 13:21:40 +0700

    Implementing milti-tier navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,23 @@
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { mount } from "marketing/MarketingApp";
 
 export const MarketingApp = () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname: currentPathname } = history.location;
+        if (currentPathname === nextPathname) return;
+
+        history.push(nextPathname);
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref}></div>;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-export const mount = (element) => {
-  ReactDOM.render(<App />, element);
+export const mount = (element, { defaultHistory, onNavigate }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} />, element);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname: currentPathname } = history.location;
+      if (currentPathname === nextPathname) return;
+
+      history.push(nextPathname);
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const root = document.getElementById("_marketing-dev-root");
-  if (root) mount(root);
+  if (root) mount(root, { defaultHistory: createBrowserHistory() });
 }
